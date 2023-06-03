8cdbb4ded93035572358f853de9af15453370e43
Chiko <53100578+chikof@users.noreply.github.com>
2023-06-03 02:44:22 +0100

    feat: nvim config

---
+++ nvim/after/plugin/formatter.lua
@@ -0,0 +1,8 @@
+require("mason-null-ls").setup({
+    ensure_installed = nil,
+    automatic_installation = false,
+    handlers = {},
+    automatic_setup = true,
+})
+
+require("null-ls").setup({})
