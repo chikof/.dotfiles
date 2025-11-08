e93288c38bf08231d55d9f862e3b2833e60b3e9c
Chiko <chiko@envs.net>
2025-11-08 16:53:39 +0000

    fix: properly inherit the settings

--- flake.nix
+++ flake.nix
@@ -39,9 +39,13 @@
       nixosConfigurations.desktop = nixpkgs.lib.nixosSystem {
         system = [ "x86_64-linux" ];
         specialArgs = {
-          inherit self inputs outputs;
-        }
-        // settings;
+          inherit
+            self
+            inputs
+            outputs
+            settings
+            ;
+        };
         modules = [
           ./configuration.nix
           home-manager.nixosModules.home-manager
